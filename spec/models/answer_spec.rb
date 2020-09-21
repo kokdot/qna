fdc50fa92b126198d72ec498d8636563b50b93c3
kokdot <kok.74@mail.ru>
2020-09-21 12:26:37 +0500

    lesson_6_5

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,15 +6,24 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
 
   describe 'Answer best_assign' do
-    # let(:user) { create(:user) }
-    # let!(:question) {create(:question, user: user)}
-    let!(:answer) { create(:answer) }#, question: question, user: user)}
-
-    # before { login(user) }
-
+    let!(:answer) { create(:answer) }
     it 'assign the best for answer' do
       answer.best_assign
       expect(answer.best).to_not be_falsy
+    end
+  end
+
+  describe 'Answer best change' do
+    let!(:question) { create(:question) }
+    let!(:answer) { create(:answer, question: question) }
+    let!(:answer_1) { create(:answer, question: question) }
+    it 'change the best for answer' do
+      answer.best_assign
+      answer_1.best_assign
+      answer.reload
+      answer_1.reload
+      expect(answer.best).to be_falsy
+      expect(answer_1.best).to_not be_falsy
     end
   end
 end
